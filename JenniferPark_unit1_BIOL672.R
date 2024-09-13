88f6e97d35015349bbc4d79a39b9a7e8f41fbc2c
jyp4505 <jyp4505@rit.edu>
2024-09-13 09:30:53 -0400

    Unit 1 - Question 3

--- JenniferPark_unit1_BIOL672.R
+++ JenniferPark_unit1_BIOL672.R
@@ -7,23 +7,34 @@ library('reshape2')
 
 #Question 2
 #Dataset of 5000 random numbers
-#Uniform distribution
+#Normal distribution
 library(ggplot2)
 
 #generating random numbers using normal distribution
 dataset<-rnorm(5000)
 #sample mean
-mean(dataset)
+mymean<-mean(dataset)
 #sample standard deviation
-sd(dataset)
+mysd<-sd(dataset)
+
+#print mean and standard deviation
+mymeansd<-data.frame(mymean, mysd)
+print(mymeansd)
 #histogram with density line using ggplot
 ggplotdata<-data.frame(value=dataset)
 myplot1<-ggplot(ggplotdata, aes(value))+
   geom_histogram(aes(y=after_stat(density)), color="purple", fill="lightpink")+
   geom_density(color="green",lwd=1)+geom_function(fun=dnorm, color="blue", lwd=1)+
   labs(title="Histogram of Normal Distribution")
-#normal curve overlay
 
+#print myplot1
 print(myplot1)
 
+#print mean and sd to an external file
+sink(file='/Users/jenniferpark/Desktop/BIOL672/Assignments/Unit_1/desc.txt')
+print(mymeansd)
+sink()
+#HOW TO REMOVE THE 1 BEFORE MEAN AND SD?????
 
+#save Rplots.pdf as histo.pdf
+ggsave(myplot1, file="histo.pdf")
