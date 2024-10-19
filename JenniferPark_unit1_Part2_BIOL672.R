fc55064556bc3c5f08c04ea995bc73204adacd9e
jyp4505 <jyp4505@rit.edu>
2024-10-18 20:02:20 -0400

    Unit 1 - Part 2 - Question 8 - Expenditures

--- JenniferPark_unit1_Part2_BIOL672.R
+++ JenniferPark_unit1_Part2_BIOL672.R
@@ -6,48 +6,49 @@
 library('ggplot2')
 library('dplyr')
 library ('dgof')
-library('readxl')
+library('readr')
 
 #QUESTION 7
-#find appropriate data set from my field (food waste) for series of multivariate statistical tests
-#found household food waste data from kaggle - https://www.kaggle.com/datasets/mutamwamataka/fw-data
+#find appropriate data set from my field (sustainability) for series of multivariate statistical tests
+#found Filipino household expenditure data from kaggle - https://www.kaggle.com/code/issatingzon/exploratory-data-analysis
+#read cvs file
+exp_data <- read_csv("/Users/jenniferpark/Desktop/BIOL672/Assignments/Unit_1/Household_Income_Exp_Data.csv")
 
-#read excel file
-fw_data <- read_excel("/Users/jenniferpark/Desktop/BIOL672/Assignments/Unit_1/FW Data new.xls")
+#extract columns of interest- household income, food expenditure, Housing and water expenditure, transportation expenditure, education expenditure, household head employment status, household size
+#extract first 300 rows as too many rows of data
+#I modified the column headers for the columns I am extracting to remove spacing/make easier to understand
 
-#extract columns of interest- household_size, income, occupation, weekly_grocery_expenditure, purchase_quantity, and waste_quantity
-#extract first 100 rows as too many rows of data
-fw_data_extracted <- fw_data[(1:100), c("Household_Size", "Income", "Occupation", "Weekly_Grocery_Expenditure", "Purchase_Quantity", "Waste_Quantity")]
-print(fw_data_extracted)
+exp_data_extracted <- exp_data[(1:300), c("household_income", "food_expenditure", "housing_water_expenditure", "transportation_expenditure", "education_expenditure", "employment_status", "household_size")]
+print(exp_data_extracted)
 
 #write table of extracted columns
-write.table(fw_data_extracted, file='/Users/jenniferpark/Desktop/BIOL672/Assignments/Unit_1/FW_Data_Tab.txt', quote=FALSE, sep=" \t ", row=FALSE)
+write.table(exp_data_extracted, file='/Users/jenniferpark/Desktop/BIOL672/Assignments/Unit_1/Exp_Data_Tab.txt', quote=FALSE, sep=",", row=FALSE)
 
-#shape data to only include certain columns - household_size, income, occupation, weekly_grocery_expenditure, purchase_quantity, and waste_quantity
-#note took out euro symbol next to income as symbol makes it difficult to shape data
 #use read.table function to read table from external file and shape data
-data <- read.table("/Users/jenniferpark/Desktop/BIOL672/Assignments/Unit_1/FW_Data_Tab.txt", header=TRUE)
-house_size = data$Household_Size
-house_income = data$Income
-house_occupation = data$Occupation
-house_grocery = data$Weekly_Grocery_Expenditure
-house_purchasequantity = data$Purchase_Quantity
-house_FW = data$Waste_Quantity
+data <- read.table("/Users/jenniferpark/Desktop/BIOL672/Assignments/Unit_1/Exp_Data_Tab.txt", header=TRUE, row.names=NULL)
+house_income = data$household_income
+food_exp = data$food_expenditure
+housing_water_exp = data$housing_water_expenditure
+transport_exp = data$transportation_expenditure
+education_exp = data$education_expenditure
+employment_status = data$employment_status
+household_size = data$household_size
 
 print(data)
-myFW.data <- data.frame(
-  house_occupation,
-  house_size,
+myexp.data <- data.frame(
+  household_size,
+  employment_status,
   house_income,
-  house_grocery,
-  house_purchasequantity,
-  house_FW
+  food_exp,
+  housing_water_exp,
+  transport_exp,
+  education_exp
 )
 
 #print data
-print(myFW.data)
-#print summary of FW data
-print(summary(myFW.data))
+print(myexp.data)
+#print summary of household expenditure data
+print(summary(myexp.data))
 
 
 #QUESTION 8
